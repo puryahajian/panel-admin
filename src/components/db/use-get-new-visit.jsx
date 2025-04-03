0dea27ce4c815fa8b1138fd7bae563e7e805d6d3
purya <porya.hajian79@gmail.com>
2025-04-03 12:22:39 +0330

    loading api...

---
+++ src/components/db/use-get-new-visit.jsx
@@ -0,0 +1,35 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import React, { useEffect } from 'react'
+import interceptor from '../lib/interceptor';
+
+function UseGetNewVisit() {
+    const queryClient = useQueryClient();
+
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['getNewVisit'],
+        queryFn: async () => {
+            const response = await interceptor.get(`course/user-course`);
+            return response.data;
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries(['getNewVisit']);
+        },
+        onError: (error) => {
+            console.log('Error fetching data:', error);
+        },
+    });
+    
+    useEffect(() => {
+        if (data) {
+            queryClient.invalidateQueries(['getNewVisit']);
+        }
+    }, [data]); 
+
+    if (isLoading) return <div>Loading...</div>;
+
+    if (error){ return <div>Error: {error?.message}</div>};
+
+    return {data}
+}
+
+export default UseGetNewVisit
--- src/components/molecules/sick/sick.jsx
+++ src/components/molecules/sick/sick.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Text from '../../atoms/text'
 import ButtonGeneral from '../../atoms/button-general'
+import GeneralModal from '../modal-general';
+import UseGetNewVisit from '../../db/use-get-new-visit';
 
 function Sick() {
+    const [openSeeComment, setSeeComment] = useState(false);
+    const { data } = UseGetNewVisit();
+    console.log(data)
     return (
         <div>
             <Text>بیماران</Text>
@@ -19,23 +24,33 @@ function Sick() {
             </div>
 
             <div className='flex py-4 bg-gray-100 rounded-lg px-2 mt-2 items-center'>
-                <div className='w-3/4 grid grid-cols-6'>
+                <div className='w-3/4 grid grid-cols-6 items-center'>
                     <Text>1</Text>
                     <Text className={`col-span-2`}>lorem ipsum</Text>
                     <Text>123456</Text>
+                    <ButtonGeneral onClick={() => setSeeComment(true)} className={` border-transparent cursor-pointer`}>
+                        نظرات
+                    </ButtonGeneral>
                 </div>
                 <div className='w-4/12 text-left flex justify-end gap-2'>
                     <ButtonGeneral className={`!py-2 bg-red-500 border-transparent cursor-default text-white`}>
                         پایان ویزیت
                     </ButtonGeneral>
-                    {/* <ButtonGeneral className={`!py-2 bg-green-500 border-transparent cursor-default text-white`}>
-                        درحال ویزیت
-                    </ButtonGeneral> */}
                     {/* <ButtonGeneral className={`!py-2 bg-yellow-400 border-transparent cursor-default text-gray-500`}>
                         منتظر تایید پزشک
                     </ButtonGeneral> */}
                 </div>
             </div>
+
+            <GeneralModal
+                open={openSeeComment}
+                handleClose={() => setSeeComment(false)}
+                title="نظرات"
+                actionText="ذخیره"
+                actionHandler={() => { setSeeComment(false); }}
+            >
+                
+            </GeneralModal>
         </div>
     )
 }

a24026c5d379d49fe16384dc7d898e0d334e7082
purya <porya.hajian79@gmail.com>
2025-04-04 20:32:38 +0330

    loading api ticket

---
+++ src/components/db/use-create-ticket.jsx
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react'
+import qs from "qs";
+import interceptor from '../../lib/interceptor';
+
+function UseCreateTicket() {
+    const queryClient = useQueryClient();
+    
+    const mutation = useMutation(
+        async ({ section, description }) => {
+
+            const data = qs.stringify({
+                title: description,
+                section: section
+            });
+            
+            const response = await interceptor.post(`ticket/tickets/`, data);
+            return response.data;
+        },
+        {
+            onSuccess: (data) => {
+                queryClient.invalidateQueries(['createTicket']);
+            },
+        }
+    );
+    
+    return mutation;
+}
+
+export default UseCreateTicket

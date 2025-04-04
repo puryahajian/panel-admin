78dbcf25d15ee475c193df5efe6a4a7b85963ada
purya <porya.hajian79@gmail.com>
2025-04-04 20:36:51 +0330

    loading api ticket

---
+++ src/components/db/use-sent-answer.jsx
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react'
+import interceptor from '../../lib/interceptor';
+
+function UseSentAnswer() {
+    const queryClient = useQueryClient();
+    
+    const mutation = useMutation(
+        async ({ getIdTicket, fileName, valueMessage }) => {
+
+            const formData = new FormData();
+            formData.append('text', valueMessage);
+            formData.append('file', fileName);
+            
+            const response = await interceptor.post(`ticket/tickets/${getIdTicket}/messages/`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data;
+        },
+        {
+            onSuccess: (data) => {
+                console.log(data)
+                // toast.success('پروفایل با موفقیت ذخیره شد')
+                queryClient.invalidateQueries(['createNewProduct']);
+            },
+        }
+    );
+    
+    return mutation;
+}
+
+export default UseSentAnswer

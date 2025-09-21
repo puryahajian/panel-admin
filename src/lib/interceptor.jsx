61b13e16bd5109b6d588ff3566254cac4586ebef
purya <porya.hajian79@gmail.com>
2025-09-21 17:01:01 +0330

    add panel-sonatikaran

--- src/lib/interceptor.jsx
+++ src/lib/interceptor.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const interceptor = axios.create({
-  baseURL: 'https://api.iranishop.om/',
+  baseURL: 'https://api.sonatikaran.ir/',
 });
 
 let isRefreshing = false;
